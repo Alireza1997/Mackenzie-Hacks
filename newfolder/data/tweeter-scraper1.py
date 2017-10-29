f65a6aeb81a5cdc0859decef4d8ab3f0bd1ba431
alireza1997 <alireza.nickooie@mail.utoronto.ca>
2017-10-28 22:16:20 -0400

    no hash data added

--- newfolder/data/tweeter-scraper1.py
+++ newfolder/data/tweeter-scraper1.py
@@ -1,6 +1,8 @@
 from subprocess import call
 
+
 query = input("query\n")
+hashtag = input("hashtag\n")
 start_date = input("start date\n")
 end_date = input("end date\n")
 limit = input("limit\n")
@@ -8,7 +10,7 @@ limit = input("limit\n")
 #%23bellletstalk%20near%3A%22Toronto%2C%20Ontario%22%20within%3A15mi&src=typd&lang=en
 #%23bellletstalk%20near%3A%22Mississauga%2C%20Ontario%22%20within%3A15mi&src=typd&lang=en
 #%23bellletstalk%20near%3A%22Mississauga%2C%20Ontario%22%20within%3A15mi%20since%3A2017-10-24%20until%3A2017-10-28&src=typd&lang=en
-
+#depression%20%23bellletstalk%20near%3A"Toronto%2C%20Ontario"%20within%3A15mi%20since%3A2017-10-18&src=typd&lang=en
 
 location = [
 "Toronto",
@@ -68,14 +70,18 @@ location = [
 
 if (start_date != -1):
 	for i in location:
-		part1 = '%23' #hashtag
-		part2 = query
-		part3 = '%20near%3A%22' #near	
-		part4 = i
-		part5 = '%2C%20Ontario%22%20within%3A15mi'
-		part6 = '%20since%3A' + start_date + '%20until%3A' + end_date #2017-10-24
-		part7 = '&src=typd&lang=en' #rest of it
-		command = part1+part2+part3+part4+part5+part6+part7
+		command = ''
+		if (query != '-1'):
+			command = command + query + '%20'
+		if (hashtag != '-1'):
+			command = command + '%23' #hashtag
+			command = command + hashtag
+		command = command + '%20near%3A%22' #near	
+		command = command + i
+		command = command + '%2C%20Ontario%22%20within%3A15mi'
+		command = command + '%20since%3A' + start_date + '%20until%3A' + end_date #2017-10-24
+		command = command + '&src=typd&lang=en' #rest of it
+		#command = part1+part2+part3+part4+part5+part6+part7
 		print (command, "\n")
 		call(["twitterscraper" , command , "-l", limit ,"-o", i+".json"])	
 
